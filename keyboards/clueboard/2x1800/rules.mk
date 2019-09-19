c1ebe358bea3a5c5f46b5ac86ed69142faf7b484
davidnite <davidnite@me.com>
2019-09-19 10:30:54 -0500

    add e65 keymap

--- keyboards/clueboard/2x1800/rules.mk
+++ keyboards/clueboard/2x1800/rules.mk
@@ -8,7 +8,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=1024
 
 
 # Build Options
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes        # Console for debug(+400)
@@ -18,3 +18,5 @@ MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 UNICODE_ENABLE = no         # Unicode
 RGBLIGHT_ENABLE = yes       # RGB on port C6
 AUDIO_ENABLE = yes          # Audio output on port C4 and B7
+
+
